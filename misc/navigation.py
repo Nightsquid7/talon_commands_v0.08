ee1b17906e89263b8c714ad31b2d5a75b36ba5f3
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2019-11-18 09:11:05 +0900

    removed doomway/jeepway

--- misc/navigation.py
+++ misc/navigation.py
@@ -8,8 +8,8 @@ keymap = {
     "scroll down": [Key("down")] * 30,
     "scroll up": [Key("up")] * 30,
     # NB home and end do not work in all apps
-    "(scroll way down | doomway)": Key("cmd-down"),
-    "(scroll way up | jeepway)": Key("cmd-up"),
+    "scroll way down": Key("cmd-down"),
+    "scroll way up": Key("cmd-up"),
     "page up": [Key("pageup")],
     "page down": [Key("pagedown")]
 }
