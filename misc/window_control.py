4149c41ffd0f3ffe1f37ec34e34243f46a7961b6
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-01-25 12:00:15 +0900

    removed unnecessary commands from window_control.py...

--- misc/window_control.py
+++ misc/window_control.py
@@ -40,10 +40,10 @@ ctx.keymap(
         "[open] launcher": Key("cmd-space"),
         "([switch] app (next | right) | switch it)": Key("cmd-tab"),
         "[switch] app (left | previous | preev)": Key("cmd-shift-tab"),
-        "[open] mission control": lambda m: macos.dock_notify("com.apple.expose.awake"),
-        "[open] launchpad": lambda m: macos.dock_notify("com.apple.launchpad.toggle"),
+        #"[open] mission control": lambda m: macos.dock_notify("com.apple.expose.awake"),
+        #"[open] launchpad": lambda m: macos.dock_notify("com.apple.launchpad.toggle"),
 		# the following requires keyboard shortcut for mission control in System Preferences > Keyboard > Shortcuts > Mission Control > Show Notification Center.
 		# is there a bundle id we can use instead?
-        "([(open | show)] notification center | ( (show | open) (today | widgets) ))": Key("shift-ctrl-f8"),
+        #"([(open | show)] notification center | ( (show | open) (today | widgets) ))": Key("shift-ctrl-f8"),
     }
 )
