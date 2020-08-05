48296c013b1317a9bda31f3750e8ba4b605569df
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-08-05 13:38:39 +0900

    t b l in max

--- apps/max.py
+++ apps/max.py
@@ -6,9 +6,10 @@ ctx = Context("max", bundle="com.cycling74.Max")
 keymap = {
     
     "print": ["n", "print"],
-    
+    "trigger event list": ["n", "t b l"],
     "(locket | locket it) | (unlock it)": [command_click],
     "inspect": [lambda m: press_key_and_click(m, "alt", 1, 1)],
+    "copy from keyboard | click4": [Key("alt-cmd-n")],
 }
 
 ctx.keymap(keymap)
