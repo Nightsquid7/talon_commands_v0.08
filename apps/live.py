5f06c05ff29920adcdc674faa6af73972cb23e34
Steven Berkowitz <stevenaaronberkowitz@gmail.com>
2020-08-07 12:27:37 +0900

    basic key commands - prefereces, save as etc

--- apps/live.py
+++ apps/live.py
@@ -19,14 +19,22 @@ def focusCategories(upCount):
        
 
 keymap = {
+    "save it": [Key("cmd-s")],
+    "save as": [Key("cmd-shift-s")],
+    "duplicate": [Key("cmd-d")],
     "browser": [Key("cmd-alt-b")],
+    "preferences": [Key("cmd-,")],
+    "rename": [Key("cmd-r")],
     "device view": [Key("cmd-alt-l")],
+    "device window": [Key("cmd-alt-p")],
     "(hi | show) video": [Key("cmd-alt-v")],
+    "lp | loup": [Key("cmd-l")],
     "(plug in) | third party": lambda m: focusCategories(3),
-    "live devices": lambda m: focusCategories(4),
+    "max devices": lambda m: focusCategories(4),
     "many effects": lambda m: focusCategories(5),
     "audio effects": lambda m: focusCategories(6),
-    "ableton instruments": lambda m: focusCategories(7), 
+    "live devices": lambda m: focusCategories(7), 
+
     "search": [Key("cmd-f")],
     "(show | hide) (sentence | seds)": [Key("cmd-alt-r")],
     "(show | hide) (devices | midi)": [Key("cmd-alt-l")],
